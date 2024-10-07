73f8c2174f0118e7b202b5f091c5cce949f6caf4
Rosmontis <rosmontis233@hotmail.com>
2024-10-07 20:04:59 +0800

    233

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import {Vue, Component} from 'vue-facing-decorator';
+import Avatar from './components/Avatar.vue';
+
+@Component({components: {Avatar}})
+export default class App extends Vue {}
+</script>
+
+<template>
+  <Avatar />
+</template>
+
+<style lang="scss">
+@import '@/css/mocha';
+
+* {
+  color: $text;
+}
+
+body {
+  background-color: $base;
+}
+
+#app {
+  width: 100%;
+  max-width: 900px;
+  min-height: 100vh;
+  margin: auto;
+}
+</style>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,57 @@
+<script lang="ts">
+import {Vue, Component} from 'vue-facing-decorator';
+
+@Component({})
+export default class Avatar extends Vue {
+
+}
+</script>
+
+<template>
+  <div class="avatar">
+    <div class="left">
+      <img alt="avatar" src="/img/ros.jpg" />
+    </div>
+    <div class="right">
+      <h1>Rosmontis Here!</h1>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.avatar {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: row;
+  flex-wrap: nowrap;
+
+  .left {
+    flex: 1;
+    display: block;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    margin: 0.5rem auto;
+
+    img {
+      object-fit: contain;
+      width: 100%;
+      height: 100%;
+      margin: auto;
+    }
+  }
+
+  .right {
+    flex: 2;
+    display: block;
+    width: fit-content;
+    height: 200px;
+    padding: calc(100px - 1rem) 0;
+
+    h1 {
+      font-size: 2rem;
+    }
+  }
+}
+</style>
