0a46a235fef4eebed8e723149624d64ffda06fb2
Rosmontis <rosmontis233@hotmail.com>
2024-10-10 17:57:33 +0800

    blog

--- src/logic/data.ts
+++ src/logic/data.ts
@@ -1,25 +1,25 @@
 export interface Link {
-  name: string;
-  url: string;
+  name: string
+  url: string
 }
 
 export interface Contact {
-  name: string;
-  icon: string;
-  url: string;
-  id: string;
+  name: string
+  icon: string
+  url: string
+  id: string
 }
 
 export interface Friend {
-  image: string;
-  name: string;
-  url: string;
-  description: string;
+  image: string
+  name: string
+  url: string
+  description: string
 }
 
 export interface Blog {
-  image: string;
-  tittle: string;
-  url: string;
-  brief: string;
+  image: string
+  tittle: string
+  url: string
+  brief: string
 }
