2c3698917df280c3dc271ccf473a66ce0ce363ee
Elihuso Quigley <Elihuso@outlook.com>
2024-11-06 17:39:23 +0800

    [+] Add image & description

--- src/components/BlogList.vue
+++ src/components/BlogList.vue
@@ -2,6 +2,7 @@
 import { dataHost } from '@/logic/config'
 import { router } from '@/router'
 import { Component, Vue } from 'vue-facing-decorator'
+import blog_image from '@/data/blog-image.json'
 
 @Component({ components: {} })
 export default class BlogList extends Vue {
@@ -18,13 +19,25 @@ export default class BlogList extends Vue {
   push(name: string) {
     router.push({ path: `/docs/${name}` })
   }
+
+  imageUrl(name: string) {
+    return blog_image[name].image
+  }
+
+  description(name: string) {
+    return blog_image[name].description
+  }
 }
 </script>
 
 <template>
   <div class="blog-list">
     <div class="blog" v-for="l of li" :key="l">
-      <h1 class="blog-title" v-on:click="push(l)">{{ l }}</h1>
+      <img class="blog-image" :src="imageUrl(l)" />
+      <div class="content">
+        <h1 class="blog-title" v-on:click="push(l)">{{ l }}</h1>
+        <p class="blog-description">{{ description(l) }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -44,16 +57,71 @@ export default class BlogList extends Vue {
   .blog {
     width: 100%;
     height: fit-content;
-    display: block;
+    display: flex;
     margin: 0 auto;
     border-bottom: 1px solid $rosewater;
     border-top: 1px solid $rosewater;
-    padding: 2rem;
+    padding: 0.2rem;
+    flex-direction: row;
+    flex-wrap: nowrap;
+    justify-content: start;
+    align-items: center;
+    gap: 2rem;
 
-    h1 {
-      font-size: 1.6rem;
-      font-weight: bold;
-      color: $text;
+    .blog-image {
+      width: 175px;
+      height: 175px;
+      border-radius: 65px;
+      border: 3px solid $rosewater;
+      transition: .5s all ease-in-out;
+
+      &:hover {
+        filter: drop-shadow(0px 0px 5px $rosewater);
+      }
+    }
+
+    .content {
+      flex: 1;
+      display: flex;
+      flex-direction: column;
+      justify-content: start;
+      align-items: start;
+      gap: 0.5rem;
+
+      h1 {
+        font-size: 1.6rem;
+        text-align: start;
+        flex: 3;
+        font-weight: bold;
+        cursor: pointer;
+        position: relative;
+
+        &:before {
+          content: '';
+          display: block;
+          position: absolute;
+          width: 100%;
+          height: 2px;
+          bottom: 0;
+          left: 0;
+          background-color: $flamingo;
+          transform: scaleX(0);
+          transition: transform 0.3s ease;
+          transform-origin: top left;
+        }
+
+        &:hover {
+          &:before {
+            transform: scaleX(1);
+          }
+        }
+      }
+
+      p {
+        font-size: 1rem;
+        flex: 5;
+        text-wrap: stable;
+      }
     }
   }
 }
